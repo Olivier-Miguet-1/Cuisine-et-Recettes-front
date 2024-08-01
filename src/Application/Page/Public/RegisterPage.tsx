e6d1f8df7f7c8ee42d8661f9bd3f1e81c6197aae
Olivier-Miguet-1 <olivier.miguet1@gmail.com>
2024-08-01 09:07:09 +0200

    input

--- src/Application/Page/Public/RegisterPage.tsx
+++ src/Application/Page/Public/RegisterPage.tsx
@@ -1,40 +1,33 @@
-import { useState, useRef } from 'react';
-import {Autocomplete, Box, Button, Flex, Input, Loader} from '@mantine/core';
+import { useState } from 'react';
+import { Box, Button, Flex, Input } from '@mantine/core';
 
 export function RegisterPage() {
-  const timeoutRef = useRef<number>(-1);
-  const [value, setValue] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState<string[]>([]);
-
-  const handleChange = (val: string) => {
-    window.clearTimeout(timeoutRef.current);
-    setValue(val);
-    setData([]);
-
-    if (val.trim().length === 0 || val.includes('@')) {
-      setLoading(false);
-    } else {
-      setLoading(true);
-      timeoutRef.current = window.setTimeout(() => {
-        setLoading(false);
-        setData(['gmail.com', 'outlook.com', 'yahoo.com'].map((provider) => `${val}@${provider}`));
-      }, 1000);
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [userName, setUserName] = useState("")
+    const [email, setEmail] = useState("")
+    const handleRegister = () => {
+        const user = {
+            firstName,
+            lastName,
+            userName,
+            email,
+        }
+        console.log(user)
     }
-  };
-  
+
   return (
     <>
       <Flex direction={"column"} align={"center"} justify={"center"}>
         <Box w={500}>
-          <Input placeholder="Prénom" size={"xl"} mb={10}/>
-          <Input placeholder="Nom" size={"xl"} mb={10}/>
-          <Input placeholder="E-mail" mb={10} size={"xl"}/>
-          <Input placeholder="Pseudo" mb={10} size={"xl"}/>
+          <Input value={firstName} onChange={(e) => setFirstName(e.target.value)} placeholder="Prénom" size={"xl"} mb={10}/>
+          <Input value={lastName} onChange={(e) => setLastName(e.target.value)} placeholder="Nom" size={"xl"} mb={10}/>
+          <Input value={userName} onChange={(e) => setUserName(e.target.value)} placeholder="Pseudo" mb={10} size={"xl"}/>
+          <Input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="E-mail" mb={10} size={"xl"}/>
           <Input placeholder="Mot de passe" mb={10} size={"xl"}/>
           <Flex justify={"end"}>
-            <Button size={"xl"}>Creer mon compte</Button>
-          </Flex>
+            <Button onClick={handleRegister} size={"xl"}>Creer mon compte</Button>
+          </Flex> 
         </Box>
       </Flex>
     </>
