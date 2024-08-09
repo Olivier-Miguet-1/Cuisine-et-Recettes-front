76c1876e99080ac73693d78929cbed186b0c1b69
Olivier-Miguet-1 <olivier.miguet1@gmail.com>
2024-08-09 15:30:39 +0200

    more anomalies

--- src/Application/Page/Private/CreateRecipePage.tsx
+++ src/Application/Page/Private/CreateRecipePage.tsx
@@ -148,12 +148,12 @@ export const CreateRecipePage = () => {
                     }
                 </Flex>
 
-                <Box mt={20} mb={40}>
+                {/* <Box mt={20} mb={40}>
                     <ButtonPrimary >Créer la recette</ButtonPrimary>
                 </Box> */}
             </div>
             <div>
-                {/* <Input placeholder="Ajouter un titre" value={titleRecipe} onChange={
+                <Input placeholder="Ajouter un titre" value={titleRecipe} onChange={
                     (event) => {
                         setTitleRecipe(event.currentTarget.value)
                     }
@@ -168,7 +168,7 @@ export const CreateRecipePage = () => {
                     }
                 />
 
-               {/*  <Flex w={500} justify={"flex-end"} direction={"column"} align={"flex-end"}>
+                <Flex w={500} justify={"flex-end"} direction={"column"} align={"flex-end"}>
                     <Title mt={40} mb={20} order={3}>Ajouter des ustensiles</Title>
 
                     <Input placeholder="Ajouter un nom" value={nameUstensil} onChange={
@@ -185,7 +185,7 @@ export const CreateRecipePage = () => {
                         (event) => {
                             setQuantityUstensil(event.currentTarget.value)
                         }
-                    }/> */}
+                    }/>
 
                     <Box mt={20}>
                         <ButtonPrimary onClickButton={addUstensil} >Ajouter ustensile</ButtonPrimary>
@@ -209,7 +209,7 @@ export const CreateRecipePage = () => {
                     }
                 </Flex>
 
-                {/* <Flex>
+                <Flex>
                     {
                         listUstensil.length > 0 &&
                         listUstensil.map((ustensil, index) => {
@@ -224,7 +224,7 @@ export const CreateRecipePage = () => {
                             )
                         })  
                     }
-                </Flex> */}
+                </Flex>
 
                 <Box mt={20} mb={40}>
                     <ButtonPrimary >Creer la recette</ButtonPrimary>
